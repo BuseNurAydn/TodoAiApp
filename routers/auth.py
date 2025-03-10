1e33c19e22c20c27ff9f10174f489c3bd445142a
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2025-03-10 15:35:13 +0300

    JWT(JSON Web Tokens) encoding-decoding kodları yazıldı.

--- routers/auth.py
+++ routers/auth.py
@@ -1,19 +1,24 @@
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from models import User
-from sqlalchemy.testing import db
-from pydantic import BaseModel , Field
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel,EmailStr, Field
 from typing import Annotated
 from starlette import status
 from sqlalchemy.orm import Session
 from database import SessionLocal
 from passlib.context import CryptContext #paraloyu şifrelemek için algoritma
+from jose import jwt,JWTError
+from datetime import timedelta, datetime, timezone
 
 
 router = APIRouter(
     prefix="/auth",  # API'nin URL yolu, yani endpointlerin başına auth koyulur
     tags=["Authentication"]  #Swaggerda başlık tagsler
 )
+
+#JWT encoding ve decoding işlemleri için
+SECRET_KEY = "acoztm3revp1vfj7ld5sz2ndg5xp79r9fnr2p4hx2dy63h6a8efhj6rm54u8evh8"
+ALGORITHM = "HS256"
 
 def get_db():  #Bu fonksiyon bize veritabanını veriyor
     db = SessionLocal()
@@ -25,6 +30,7 @@ def get_db():  #Bu fonksiyon bize veritabanını veriyor
 db_dependency = Annotated[Session, Depends(get_db)]  #Dependency Injection
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #bcrypt şifreleme algoritması
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")  #token makanizmasını tanımladık
 
 #request body
 class UserRequest(BaseModel):
@@ -36,9 +42,39 @@ class UserRequest(BaseModel):
     is_active : bool
     role : str
 
+class LoginSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+#JWT(JSON Web Tokens) -encoding                                   #Ne zaman geçerliliğini kaybedecek
+def create_access_token(email: str, user_id: int, role: str, expires_delta: timedelta ):
+    payload = {'sub': email,'id': user_id, 'role': role} #sabit bir şey değil, istediğimiz özellikleri koyduk
+    expires = datetime.now(timezone.utc) + expires_delta #sistem çalıştıktan sonra timedelta da verilen zaman kadar
+    payload.update({'exp': expires})
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+#JWT(JSON Web Tokens) -decoding                     #Tokeni aldık
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])  # Token çözümleme
+        email: str = payload.get("sub")  # Email'i al
+        user_id: int = payload.get("id")  # Kullanıcı ID'sini al
+        role: str = payload.get("role")  # Kullanıcı rolünü al
+
+        if email is None or user_id is None:
+              raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+         return {"email": email, "id": user_id, "role": role}  # Kullanıcı bilgilerini döndür
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
 
 #Kullacıyı doğrulamak için
-def authenticate_user(email : str, password : str):
+def authenticate_user(email : str, password : str,db):
     user = db.query(User).filter(User.email == email).first()
     if not user:
         return False
@@ -62,3 +98,10 @@ async def create_user(db: db_dependency,user_request : UserRequest):
     db.commit()
 
 
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: LoginSchema,db: db_dependency):
+    user = authenticate_user(form_data.email, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
+    token = create_access_token(user.email, user.id, user.role, timedelta(minutes=60)) #email ile token oluşturduk
+    return {"access_token": token, "token_type": "bearer"}
